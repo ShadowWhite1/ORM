0274de31d6837763494e8b3cd9a9c3f2bdef3452
Eldar Rizaev <eldarrizaevsw@mail.ru>
2022-12-13 22:13:57 +0300

    add

---
+++ main.py
@@ -0,0 +1,64 @@
+import sqlalchemy
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship
+
+# DSN = "postgresql://postgres:@localhost:5432/netology_db1"
+# engine = sqlalchemy.create_engine(DSN)
+# connection = engine.connect()
+
+
+# metadata = sq.MetaData()
+
+
+Base = declarative_base()
+
+class Publisher(Base):
+    __tablename__ = "publisher"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.Text, unique=True)
+
+class Shop(Base):
+    __tablename__ = "shop"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.Text, unique=True)
+
+    def __str__(self):
+        return f'{self.id}: {self.name}'
+
+class Book(Base):
+    __tablename__ = "book"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.Text, nullable=False)
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
+
+    publisher = relationship(Publisher, backref="book")
+
+class Stock(Base):
+    __tablename__ = "stock"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    count = sq.Column(sq.Integer, nullable=False, default=0)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
+
+    book = relationship(Book, backref="stock")
+    shop = relationship(Shop, backref="stock")
+
+class Sale(Base):
+    __tablename__ = "sale"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.DECIMAL, nullable=False)
+    date_sale = sq.Column(sq.DateTime, nullable=False)
+    count = sq.Column(sq.Integer, nullable=False, default=1)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
+
+    stock = relationship(Stock, backref="sale")
+
+
+def create_tables(engine):
+    #Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
